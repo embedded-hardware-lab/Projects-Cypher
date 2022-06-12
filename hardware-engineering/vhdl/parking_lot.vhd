daca71f36cd16a1f17ec380ecda9099d62e396b7
Adib6637 <sheikh-muhammad-adib.bin-sh-abu-bakar@stud.hshl.de>
2022-06-12 16:58:19 +0200

    integrate

--- hardware-engineering/vhdl/parking_lot.vhd
+++ hardware-engineering/vhdl/parking_lot.vhd
@@ -1,14 +1,24 @@
-Library IEEE ;
-USE IEEE.Std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 
 entity parkingLotSystem is
  	port (	rst, clk: in std_logic;
 		input : in std_logic_vector(3 downto 0); 
 		
 		counter : out std_logic_vector(7 downto 0); 
-		gate, led : out std_logic;
-		ready : in std_logic);
+		led_red : out std_logic;
+		gate_close : out std_logic;
+		led_green : out std_logic;
+		gate_open : out std_logic;
+		ready : in std_logic;
+		
+		digit3port : out bit_vector(6 downto 0);
+		digit2port : out bit_vector(6 downto 0);
+		digit1port : out bit_vector(6 downto 0)
+		);
+		
 end entity parkingLotSystem;
 
 
@@ -16,14 +26,39 @@ architecture behavioral of parkingLotSystem is
  	type state is (idle, display, full);
  	signal current_state, next_state: state;				
 	signal counter_s: std_logic_vector(7 downto 0):= x"00";
+	signal counter_s_bit: bit_vector(7 downto 0);
 	signal led_s, gate_s : std_logic := '1';  
 	signal space_state : std_logic;
 
+	signal bcd_3 : bit_vector(3 downto 0);
+	signal bcd_2 : bit_vector(3 downto 0);
+	signal bcd_1 : bit_vector(3 downto 0);
+	signal bcd_full : bit_vector(11 downto 0);
 
 
+	component bcd_8_bit is
+    		port (	input_8_bit:    in  bit_vector (7 downto 0);
+        		bcd:    out bit_vector (11 downto 0)
+    	);
+	end component;
+
+	component LED_7segment is
+	Port ( 	input : in bit_vector (3 downto 0);
+		LED_out : out bit_vector (6 downto 0));
+	end component;
 
 
 	begin
+		converter1 : bcd_8_bit port map ( 	input_8_bit => counter_s_bit,
+							bcd => bcd_full );
+
+		digit3 : LED_7segment port map (	input => bcd_3,
+							LED_out => digit3port);
+		digit2 : LED_7segment port map (	input => bcd_2,
+							LED_out => digit2port);
+		digit1 : LED_7segment port map (	input => bcd_1,
+							LED_out => digit1port);
+
 		state_memory: process(clk, rst)
  		begin
  			if rst = '1' then
@@ -116,10 +151,15 @@ architecture behavioral of parkingLotSystem is
 			
 		end process;
 		
+
+		counter_s_bit <= to_bitvector(counter_s);
 		counter <= counter_s;
-		gate <= gate_s;
-		led <= led_s;
+		gate_open <= gate_s;
+		led_green <= led_s;
+		gate_close <= not gate_s;
+		led_red <= not led_s;
+
+		bcd_3 <= bcd_full(11 downto 8);
+		bcd_2 <= bcd_full(7 downto 4);
+		bcd_1 <=  bcd_full(3 downto 0);
 end architecture;
-
-
-
