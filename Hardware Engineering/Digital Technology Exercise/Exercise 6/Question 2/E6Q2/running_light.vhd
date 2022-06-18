1a3add1fb2264573939fc6e55db051e8761f4f5a
Adib6637 <72360300+Adib6637@users.noreply.github.com>
2022-06-18 10:54:32 +0200

    update

--- Hardware Engineering/Digital Technology Exercise/Exercise 6/Question 2/E6Q2/running_light.vhd
+++ Hardware Engineering/Digital Technology Exercise/Exercise 6/Question 2/E6Q2/running_light.vhd
@@ -11,6 +11,8 @@ architecture behavioural of running_light_8_bit is
 	signal clock_s : bit;
 	signal clock_s2 : bit_vector(7 downto 0);
 	signal clock_s3 : bit_vector(7 downto 0);
+	signal clock_s4 : bit_vector(7 downto 0);
+	signal clock_s5 : bit_vector(7 downto 0);
 	
 	component clock_divider is
 		port (	CLK_main : in bit;				
@@ -25,7 +27,13 @@ architecture behavioural of running_light_8_bit is
 		clockDevider2 : clock_divider port map(	CLK_main => clock_s2(7),				
 							CLK_N => clock_s3,
 							reset => RST );
-		clockDevider3 : clock_divider port map(	CLK_main => clock_s3(1),				
+		clockDevider3 : clock_divider port map(	CLK_main => clock_s3(7),				
+							CLK_N => clock_s4,
+							reset => RST );
+		clockDevider4 : clock_divider port map(	CLK_main => clock_s4(7),				
+							CLK_N => clock_s5,
+							reset => RST );
+		clockDevider5 : clock_divider port map(	CLK_main => clock_s5(7),				
 							CLK_N => LED_OUT,
 							reset => RST );
 		
